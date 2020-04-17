59864a0c8c2f96756972259403c5698df8f9ddce
Willian Batista <wmmbwillian@gmail.com>
2020-04-16 22:30:31 -0300

    update api to env config

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://be-the-hero-123.herokuapp.com',
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
